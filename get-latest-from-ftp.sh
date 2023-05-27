745f0440357c30e7da82089e0ce93a1edc2e2150
root <root@web.ngtech.home>
2023-05-27 13:34:29 +0300

    Added couple mirrors to the sources

--- get-latest-from-ftp.sh
+++ get-latest-from-ftp.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Canada
+FTP_HTTP_MIRROR="http://squid.mirror.globo.tech/squid/"
+
+# Netherlands
+FTP_MIRROR="ftp://ftp.nic.surfnet.nl/mirror/internet/squid/squid/"
+
+# Morrocco
 FTP_HTTP_MIRROR="http://mirror.marwan.ma/squid/software/"
 
 LATEST_FILE=$(curl -s "${FTP_HTTP_MIRROR}" | egrep -o "squid\-[0-9\.]+tar\.(xz|gz|bz2)" | tail -1)
